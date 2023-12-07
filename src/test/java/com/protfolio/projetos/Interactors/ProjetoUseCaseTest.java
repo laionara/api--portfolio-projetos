5b9530dffdc620cc8f2cca3f40d35ce5ced0cc1b
Laionara <layonara.lay@gmail.com>
2023-12-07 09:31:54 -0300

    implementação de testes

---
+++ src/test/java/com/protfolio/projetos/Interactors/ProjetoUseCaseTest.java
@@ -0,0 +1,36 @@
+package com.protfolio.projetos.Interactors;
+
+
+import com.protfolio.projetos.entities.enun.StatusEnum;
+import com.protfolio.projetos.interactors.ProjetoUseCase;
+import com.protfolio.projetos.interactors.exception.BusinessException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+@SpringBootTest
+public class ProjetoUseCaseTest {
+
+    @InjectMocks
+    ProjetoUseCase instancia;
+    @Test
+    public void test_validarExclusaoStatusIniciado(){
+        Assertions.assertThrows(BusinessException.class, () -> instancia.validarExclusao(StatusEnum.INICIADO));
+    }
+
+    @Test
+    public void test_validarExclusaoStatusEmAndamento(){
+        Assertions.assertThrows(BusinessException.class, () -> instancia.validarExclusao(StatusEnum.EM_ANDAMENTO));
+    }
+
+    @Test
+    public void test_validarExclusaoStatusEncerrado(){
+        Assertions.assertThrows(BusinessException.class, () -> instancia.validarExclusao(StatusEnum.ENCERRADO));
+    }
+
+
+}
+
